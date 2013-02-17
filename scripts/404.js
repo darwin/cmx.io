482aa7c4e84b8b8eaffc555dc6613f4b3dcc01c4
Antonin Hildebrand <antonin@hildebrand.cz>
2013-02-17 01:01:18 +0100

    improve 404 page, also reload cmx.io when hash changes (manual editing)

--- scripts/404.js
+++ scripts/404.js
@@ -5,6 +5,16 @@
 
   if (m) {
     window.location = "/#" + m[1];
+    return;
   }
+
+  m = location.pathname.match(/^\/error\/([^#?\/]+)/);
+
+  if (m) {
+    window.location = "/#" + m[1];
+    return;
+  }
+
+  document.getElementById("body").style.display = "block";
 
 }).call(this);
